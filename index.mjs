39fffe1f3e8962d131a68a72738b905fa9500ff1
Tyler Torola <Tyler@TJT.Codes>
2019-03-28 08:12:11 -0400

    Add basic hash router

--- index.mjs
+++ index.mjs
@@ -1,3 +1,70 @@
-import { h, render } from './preact.mjs';
+import { Component, h, render } from './preact.mjs';
 
-render(h('div', {}, 'Hello world'), document.body);
+const getDisplayName = WrappedComponent => (
+  WrappedComponent.displayName || WrappedComponent.name || 'Component'
+);
+
+const getLocation = () => (
+  window.location.hash.length > 0
+    ? window.location.hash.slice(1)
+    : ''
+);
+
+const withRoute = WrappedComponent => {
+  class WithRoute extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        route: getLocation()
+      }
+
+      this.onLocationChange = this.onLocationChange.bind(this);
+    }
+
+    componentDidMount() {
+      window.addEventListener('hashchange', this.onLocationChange);
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('hashchange', this.onLocationChange);
+    }
+
+    onLocationChange() {
+      this.setState({
+        route: getLocation()
+      });
+    }
+
+    render(props, { route }) {
+      return h(WrappedComponent, { ...props, route });
+    }
+  }
+
+  WithRoute.displayName = `withRoute(${getDisplayName(WrappedComponent)})`;
+  return WithRoute;
+}
+
+const Root = withRoute(({ route }) => {
+  switch (route) {
+    case "":
+      return h('div', {}, [
+        h('div', {}, 'Test Link'),
+        h('a', { href: '#foo' }, 'Foo')
+      ]);
+    case "foo":
+      return h('div', {}, [
+        h('a', { href: '#' }, 'Home')
+      ]);
+    default:
+      throw new Error('ROUTE_NOT_FOUND');
+  }
+});
+
+const main = () => {
+  render(h(Root, {}), document.body);
+}
+
+export {
+  main
+};
--- preact.mjs
+++ preact.mjs
@@ -1,11 +1,7 @@
 import {
   h,
-  h as createElement,
-  cloneElement,
   Component,
   render,
-  rerender,
-  options
 } from 'https://cdn.jsdelivr.net/npm/preact/dist/preact.mjs';
 
 export { h, Component, render };
