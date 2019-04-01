1c9e0215e132cc67acd703e2efaa890cc4752950
Tyler Torola <Tyler@TJT.Codes>
2019-04-01 09:08:38 -0400

    Add canvas glue code

--- amaze/canvas-grid.mjs
+++ amaze/canvas-grid.mjs
@@ -1,3 +1,40 @@
-import { h } from './preact.mjs';
+import { Component, h } from './preact.mjs';
 
-export const Grid = () => h('div', {}, 'canvas-grid');
+/**
+ * type Fill = string
+ * type Maze = {
+ *   maxHeight: number,
+ *   maxWidth: number,
+ *   rows: Array<Array<Fill>>,
+ *   walls: {
+ *     vertical: Array<Array<boolean>>,
+ *     horizontal: Array<Array<boolean>>
+ *   }
+ * }
+ */
+
+export class Grid extends Component {
+  componentDidMount() {
+    renderCanvas({
+      canvas: this.canvas,
+      ctx: this.canvas.getContext('2d'),
+      maze: this.props.maze
+    });
+  }
+
+  componentDidUpdate() {
+    renderCanvas({
+      canvas: this.canvas,
+      ctx: this.canvas.getContext('2d'),
+      maze: this.props.maze
+    });
+  }
+
+  render() {
+    return h('canvas', { ref: r => this.canvas = r });
+  }
+}
+
+const renderCanvas = ({ ctx, canvas, maze }) => {
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
