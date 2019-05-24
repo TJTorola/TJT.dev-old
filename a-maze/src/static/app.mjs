c65ed6828837eb5ae3c1c67703b135a096b457a3
Tyler Torola <Tyler@TJT.Codes>
2019-05-24 15:19:10 -0400

    Strip out all canvas management code
    We're starting fresh again

--- a-maze/src/static/app.mjs
+++ a-maze/src/static/app.mjs
@@ -1,187 +1,8 @@
 import { SCHEME as SC } from "./constants.mjs";
-import {
-  useAnimationFrame,
-  useLocation,
-  useRedux,
-  useStyle,
-  getHashRoute
-} from "./hooks.mjs";
+import { useLocation, useStyle, getHashRoute } from "./hooks.mjs";
 import * as icons from "./icons.mjs";
 import { Loader } from "./loader.mjs";
-import { h, useCallback, useEffect, useRef, useState } from "./react.mjs";
-import { clamp, m } from "./lib/util.mjs";
-
-const mazeManager = ({ bridge, ctx }) => {
-  const setImage = () => {
-    if (ctx.current) {
-      const { wasm, maze } = bridge.current;
-      const imageData = new ImageData(
-        new Uint8ClampedArray(
-          wasm.memory.buffer,
-          maze.image_data(),
-          maze.width() * maze.height() * 4
-        ),
-        maze.width(),
-        maze.height()
-      );
-
-      ctx.current.putImageData(imageData, 0, 0);
-    }
-  };
-
-  return store => next => action => {
-    if (!bridge.current) return next(action);
-
-    switch (action.type) {
-      case "LOADED": {
-        setImage();
-        break;
-      }
-
-      case "GENERATOR": {
-        const { maze } = bridge.current;
-        maze.set_generator(action.payload);
-        setImage();
-        store.dispatch({ type: "STEP_COUNT", payload: maze.step_count() });
-        break;
-      }
-
-      case "STEP": {
-        const { maze } = bridge.current;
-        maze.set_step(action.payload);
-        setImage();
-        break;
-      }
-
-      case "LOCATION": {
-        const loc = action.payload;
-        const { Generator } = bridge.current;
-        const generator = {
-          hilburt: Generator.Hilburt,
-          random: Generator.Random,
-          test: Generator.Test
-        }[loc.params.generator];
-
-        if (generator !== undefined) {
-          store.dispatch({ type: "GENERATOR", payload: generator });
-        }
-        break;
-      }
-    }
-
-    next(action);
-  };
-};
-
-const INITIAL_STATE = { loading: true };
-const reducer = (mazeState = INITIAL_STATE, action) => {
-  const setField = field => () => ({
-    ...mazeState,
-    [field]: action.payload
-  });
-
-  const ifLoaded = cb => {
-    if (!mazeState.loading) {
-      return cb();
-    } else {
-      throw new Error(`Cannot dispatch ${action.type} when maze is not loaded`);
-    }
-  };
-
-  switch (action.type) {
-    case "GENERATOR":
-      return ifLoaded(() => ({
-        ...mazeState,
-        generator: action.payload,
-        playing: false,
-        step: 0
-      }));
-
-    case "LOADED": {
-      const maze = action.payload;
-
-      return {
-        height: maze.height(),
-        loading: false,
-        playing: false,
-        step: 0,
-        stepCount: maze.step_count(),
-        width: maze.width(),
-        generator: mazeState.generator
-      };
-    }
-
-    case "PLAYING":
-      return ifLoaded(setField("playing"));
-
-    case "PLAYING_INTERVAL":
-      return ifLoaded(setField("playingInterval"));
-
-    case "STEP":
-      return ifLoaded(setField("step"));
-
-    case "STEP_COUNT":
-      return ifLoaded(setField("stepCount"));
-
-    default:
-      return mazeState;
-  }
-};
-
-const useMaze = ({ cellSize, wallSize, contentSize }) => {
-  const bridge = useRef();
-  const ctx = useRef();
-  const canvasRef = useCallback(canvas => {
-    if (canvas) {
-      ctx.current = canvas.getContext("2d");
-      ctx.current.imageSmoothingEnabled = false;
-    }
-  }, []);
-
-  const store = useRedux(reducer, [mazeManager({ ctx, bridge })]);
-
-  useEffect(() => {
-    // Allow the app to calculate w & h before initializing the maze
-    if (!contentSize) return;
-
-    // Don't recalculate if size changes
-    if (!store.getState().loading) return;
-
-    (async () => {
-      const pkg = await import("./pkg/index.js");
-      const wasm = await pkg.default("./pkg/index_bg.wasm");
-      pkg.a_maze_init();
-      const maze = pkg.Maze.new(
-        cellSize,
-        wallSize,
-        contentSize.width - 64,
-        contentSize.height - 64
-      );
-      bridge.current = { wasm, maze, Generator: pkg.Generator };
-      store.dispatch({ type: "LOADED", payload: maze });
-    })();
-  }, [contentSize]);
-
-  useAnimationFrame(() => {
-    const state = store.getState();
-
-    if (state.playing) {
-      const { step, stepCount } = state;
-
-      if (step < stepCount - 1) {
-        bridge.current.maze.set_step(step + 1);
-        store.dispatch({ type: "STEP", payload: step + 1 });
-      } else {
-        store.dispatch({ type: "PLAYING", payload: false });
-      }
-    }
-  });
-
-  return {
-    canvasRef,
-    store
-  };
-};
+import { h } from "./react.mjs";
 
 const STYLE = `
 hr {
@@ -291,30 +112,6 @@ const LiAnchor = ({ children, href }) =>
 export const App = () => {
   const loc = useLocation();
   const classes = useStyle(STYLE);
-  const contentRef = useRef();
-  const [contentSize, setContentSize] = useState(null);
-  useEffect(() => {
-    // Wait until content class exists to set contentSize
-    if (!!classes.content) {
-      setContentSize({
-        width: contentRef.current.clientWidth,
-        height: contentRef.current.clientHeight
-      });
-    }
-  }, [classes]);
-
-  const {
-    store: { dispatch, getState },
-    canvasRef
-  } = useMaze({
-    cellSize: 16,
-    wallSize: 4,
-    contentSize
-  });
-
-  useEffect(() => {
-    dispatch({ type: "LOCATION", payload: loc });
-  }, [loc]);
 
   return h(
     "main",
@@ -326,25 +123,15 @@ export const App = () => {
       h(
         "button",
         {
-          className: classes.control,
-          onClick: () =>
-            dispatch({ type: "PLAYING", payload: !getState().playing })
+          className: classes.control
         },
-        h(getState().playing ? icons.Pause : icons.Play, { size: 23 })
+        h(false ? icons.Pause : icons.Play, { size: 23 })
       ),
       h("input", {
         className: classes.slider,
         type: "range",
         min: 0,
-        max: getState().stepCount ? getState().stepCount - 1 : 0,
-        value: getState().step || 0,
-        onChange: e => {
-          const stepNum = parseInt(e.target.value);
-          const nextStep = clamp(0, getState().stepCount)(stepNum);
-          if (getState().step !== nextStep) {
-            dispatch({ type: "STEP", payload: nextStep });
-          }
-        }
+        max: 0
       })
     ),
     h(
@@ -364,16 +151,6 @@ export const App = () => {
         h(LiAnchor, { href: getHashRoute({ generator: "test" }) }, "Test")
       )
     ),
-    h(
-      "section",
-      { className: classes.content, ref: contentRef },
-      getState().loading
-        ? h(Loader)
-        : h("canvas", {
-            ref: canvasRef,
-            width: getState().width,
-            height: getState().height
-          })
-    )
+    h("section", { className: classes.content }, h(Loader))
   );
 };
