716529ecb6e61ccbfd2ee03f2749f73bf096a965
Tyler Torola <Tyler@TJT.Codes>
2019-06-06 22:46:30 -0400

    Un classify the AMaze class
    This has no use being wrapped in individual instances because two would
    always end up fighting in the dom. Makes more sense as global functions.

--- a-maze/src/static/main.js
+++ a-maze/src/static/main.js
@@ -2,69 +2,75 @@ const CELL_SIZE = 10;
 const WALL_SIZE = 1;
 const PADDING = 64;
 
-class AMaze {
-  constructor() {
-    this.elements = {
-      app: document.getElementById("app"),
-      contentWrapper: document.getElementById("content-wrapper"),
-      playButton: document.getElementById("play-button"),
-      stepSlider: document.getElementById("step-slider"),
-      canvasBg: document.getElementById("canvas-bg")
-    };
+const ELEMENTS = {
+  app: document.getElementById("app"),
+  contentWrapper: document.getElementById("content-wrapper"),
+  playButton: document.getElementById("play-button"),
+  stepSlider: document.getElementById("step-slider"),
+  canvasBg: document.getElementById("canvas-bg")
+};
 
-    this.handlers = {
-      INITIALIZED: this.initialized.bind(this),
-      SETUP_COMPLETE: this.setupComplete.bind(this),
-    };
+// ----------------
+// UTILITIES -------------
+// ---------------------------------------------------
 
-    this.worker = new Worker("./worker.js");
-    this.worker.onmessage = message => {
-      const handler = this.handlers[message.data.type];
-
-      if (!handler) {
-        throw new Error(`Un-handled message, '${data.type}'`);
-      } else {
-        handler(message.data.payload);
-      }
-    };
-  }
-
-  getGenerator() {
-    const { hash } = window.location;
-    const route = hash.length > 0 ? hash.slice(1) : "";
-    return route.split("/")[0];
-  }
-
-  initialized(payload) {
-    if (!payload.success) {
-      console.error("Could not initialize WASM");
-      throw new Error(payload.error);
-    }
-
-    this.worker.postMessage({
-      type: "SETUP",
-      payload: {
-        cellSize: CELL_SIZE,
-        wallSize: WALL_SIZE,
-        maxHeight: this.elements.contentWrapper.clientHeight - PADDING,
-        maxWidth: this.elements.contentWrapper.clientWidth - PADDING,
-        generator: this.getGenerator(),
-      }
-    });
-
-    window.addEventListener("hashchange", () => {
-      this.worker.postMessage({
-        type: "SET_GENERATOR",
-        payload: this.getGenerator()
-      });
-    });
-  }
-
-  setupComplete({ width, height }) {
-    this.elements.canvasBg.width = width;
-    this.elements.canvasBg.height = height;
-    this.elements.app.setAttribute("data-status", 'loaded');
-  }
+const getGenerator = () => {
+  const { hash } = window.location;
+  const route = hash.length > 0 ? hash.slice(1) : "";
+  return route.split("/")[0];
 }
 
-window.aMaze = new AMaze();
+// ----------------
+// MSG HANDLERS ----------
+// ---------------------------------------------------
+
+const initialized = (payload) => {
+  if (!payload.success) {
+    console.error("Could not initialize WASM");
+    throw new Error(payload.error);
+  }
+
+  WORKER.postMessage({
+    type: "SETUP",
+    payload: {
+      cellSize: CELL_SIZE,
+      wallSize: WALL_SIZE,
+      maxHeight: ELEMENTS.contentWrapper.clientHeight - PADDING,
+      maxWidth: ELEMENTS.contentWrapper.clientWidth - PADDING,
+      generator: getGenerator(),
+    }
+  });
+
+  window.addEventListener("hashchange", () => {
+    WORKER.postMessage({
+      type: "SET_GENERATOR",
+      payload: getGenerator()
+    });
+  });
+}
+
+const setupComplete = ({ width, height }) => {
+  ELEMENTS.canvasBg.width = width;
+  ELEMENTS.canvasBg.height = height;
+  ELEMENTS.app.setAttribute("data-status", 'loaded');
+}
+
+const HANDLERS = {
+  INITIALIZED: initialized,
+  SETUP_COMPLETE: setupComplete,
+};
+
+// ----------------
+// WORKER SETUP ----------
+// ---------------------------------------------------
+
+const WORKER = new Worker("./worker.js");
+WORKER.onmessage = message => {
+  const handler = HANDLERS[message.data.type];
+
+  if (!handler) {
+    throw new Error(`Un-handled message, '${data.type}'`);
+  } else {
+    handler(message.data.payload);
+  }
+};
