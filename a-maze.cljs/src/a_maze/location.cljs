66ffab94276a1796ed27458f9775e417e475f551
Tyler Torola <Tyler@TJT.Codes>
2019-08-15 10:31:41 -0400

    Add tests for a more fleshed out parse-location fn

--- a-maze.cljs/src/a_maze/location.cljs
+++ a-maze.cljs/src/a_maze/location.cljs
@@ -1,5 +1,5 @@
 (ns a-maze.location
-  (:require [cljs.test :refer-macros [deftest is run-tests]]))
+  (:require [cljs.test :refer-macros [deftest is]]))
 
 (defn parse-location-data [hash-loc]
   (if (= 0 (count hash-loc))
@@ -12,11 +12,34 @@
   (is (= {:generator nil}
          (parse-location-data ""))))
 
-(defn handle-hash-change []
-  (js/console.log js/window.location.hash))
+(defn parse-location [routes current-route])
 
-(.addEventListener
- js/window
- "hashchange"
- handle-hash-change
- false)
+(deftest test-location-map
+  (def test-routes {:group "/o/:org-id/g/:group-id"
+                    :group-foo "/o/:org-id/g/:group-id/foo"
+                    :org-settings "/o/:org-id/settings"
+                    :profile-settings "/profile-settings"})
+
+  (is (= (parse-location test-routes "#/o/123/g/456/foo")
+         {:route :group-foo
+          :params {:org-id "123"
+                   :group-id "456"}}))
+  (is (= (parse-location test-routes "#/o/889/g/909")
+         {:route :group
+          :params {:org-id "889"
+                   :group-id "909"}}))
+  (is (= (parse-location test-routes "#/o/345/settings")
+         {:route :org-settings
+          :params {:org-id "345"}}))
+  (is (= (parse-location test-routes "#/profile-settings")
+         {:route :profile-settings
+          :params {}}))
+  (is (= (parse-location test-routes "")
+         {:route nil
+          :params {}}))
+  (is (= (parse-location test-routes "#")
+         {:route nil
+          :params {}}))
+  (is (= (parse-location test-routes "#/")
+         {:route nil
+          :params {}})))
