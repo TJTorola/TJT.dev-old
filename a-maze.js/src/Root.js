3a4a3bdd57756a48b60aa29636f7826aa2bbc5fd
Tyler Torola <Tyler@TJT.Codes>
2019-11-05 10:02:46 -0500

    Add top level tabs

--- a-maze.js/src/Root.js
+++ a-maze.js/src/Root.js
@@ -1,5 +1,5 @@
 import { createElement as h } from 'react';
-import { Button } from '@blueprintjs/core';
+import { Button, Tab, Tabs } from '@blueprintjs/core';
 
 const App = () => (
   h('main', { className: 'bp3-dark Wrapper' },
@@ -7,6 +7,10 @@ const App = () => (
       h('div', { className: 'Navbar-header' },
         h('h2', { className: 'bp3-heading' }, 'A-Maze'),
         h(Button, { icon: 'menu-closed', minimal: true })
+      ),
+      h(Tabs, { className: 'Navbar-tabs' },
+        h(Tab, { id: 'generators', title: 'Generators' }),
+        h(Tab, { id: 'solvers', title: 'Solvers' }),
       )
     ),
   )
