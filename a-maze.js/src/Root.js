cd96312aa6b2b14dcb8a6abf113d862f0fd5df3e
Tyler Torola <Tyler@TJT.Codes>
2019-11-05 09:57:28 -0500

    Add collapse menu button

--- a-maze.js/src/Root.js
+++ a-maze.js/src/Root.js
@@ -1,9 +1,13 @@
 import { createElement as h } from 'react';
+import { Button } from '@blueprintjs/core';
 
 const App = () => (
-  h('main', { className: 'bp3-dark' },
-    h('nav', {},
-      h('h2', { className: 'bp3-heading' }, 'A-Maze'),
+  h('main', { className: 'bp3-dark Wrapper' },
+    h('nav', { className: 'Navbar' },
+      h('div', { className: 'Navbar-header' },
+        h('h2', { className: 'bp3-heading' }, 'A-Maze'),
+        h(Button, { icon: 'menu-closed', minimal: true })
+      )
     ),
   )
 );
