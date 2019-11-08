e0fb5b467a101fbf7895f396cd8c15211f099e6d
Tyler Torola <Tyler@TJT.Codes>
2019-11-08 10:10:11 -0500

    Rename Navbar -> SideBar
    Blueprintjs calls the top floating bar a Navbar, renaming the side
    navigation bar to SideBar to avoid confusion before adding the former.

--- a-maze.js/src/app.js
+++ a-maze.js/src/app.js
@@ -4,81 +4,6 @@ import { Tab, Tabs } from '@blueprintjs/core';
 import { RouteContext } from './context.js';
 import { cx, getCurrentRoute, getRoute, ROUTES } from './lib.js';
 
-class Navbar extends Component {
-  state = {
-    tab: 'generators'
-  }
-
-  onGeneratorChange = generator => {
-    window.location.hash = getRoute(ROUTES.GENERATOR, { generator });
-  }
-
-  onSolverChange = solver => {
-    window.location.hash = getRoute(ROUTES.SOLVER, { solver });
-  }
-
-  setTab = tab => {
-    this.setState({ tab });
-  }
-
-  renderAlgorithms() {
-    switch (this.state.tab) {
-      case 'generators': {
-        return (
-          h(RouteContext.Consumer, {}, ({ params }) => (
-            h(Tabs, {
-              className: 'Navbar-algorithms',
-              onChange: this.onGeneratorChange,
-              selectedTabId: params.generator,
-              vertical: true,
-            },
-              h(Tab, { id: 'depth-first', title: 'Depth-first Search' }),
-              h(Tab, { id: 'kruskals', title: "Kruskal's Algorithm" }),
-              h(Tab, { id: 'prims', title: "Prim's Algorithm" }),
-            )
-          ))
-        );
-      }
-
-      case 'solvers': {
-        return (
-          h(RouteContext.Consumer, {}, ({ params }) => (
-            h(Tabs, {
-              className: 'Navbar-algorithms',
-              onChange: this.onSolverChange,
-              selectedTabId: params.solver,
-              vertical: true,
-            },
-              h(Tab, { id: 'breadth-first', title: 'Breadth-first Search' }),
-              h(Tab, { id: 'wall-follower', title: 'Wall Follower' }),
-              h(Tab, { id: 'dead-end-filling', title: 'Dead-end Filling' }),
-            )
-          ))
-        );
-      }
-
-      default: {
-        throw new Error(`Un-handled case '${this.state.tab}'`);
-      }
-    }
-  }
-
-  render() {
-    return (
-      h('nav', { className: 'Navbar' },
-        h('div', { className: 'Navbar-header' },
-          h('h2', { className: 'bp3-heading' }, 'A-Maze'),
-        ),
-        h(Tabs, { className: 'Navbar-tabs', onChange: this.setTab },
-          h(Tab, { id: 'generators', title: 'Generators' }),
-          h(Tab, { id: 'solvers', title: 'Solvers' }),
-        ),
-        this.renderAlgorithms()
-      )
-    );
-  }
-};
-
 export class Root extends Component {
   state = {
     darkMode: true,
@@ -90,7 +15,7 @@ export class Root extends Component {
     return (
       h(RouteProvider, {}, 
         h('main', { className: cx({ Wrapper: true, 'bp3-dark': darkMode }) },
-          h(Navbar)
+          h(SideBar)
         )
       )
     );
@@ -118,3 +43,79 @@ class RouteProvider extends Component {
     return h(RouteContext.Provider, { value: this.state.route }, this.props.children);
   }
 }
+
+class SideBar extends Component {
+  state = {
+    tab: 'generators'
+  }
+
+  onGeneratorChange = generator => {
+    window.location.hash = getRoute(ROUTES.GENERATOR, { generator });
+  }
+
+  onSolverChange = solver => {
+    window.location.hash = getRoute(ROUTES.SOLVER, { solver });
+  }
+
+  setTab = tab => {
+    this.setState({ tab });
+  }
+
+  renderAlgorithms() {
+    switch (this.state.tab) {
+      case 'generators': {
+        return (
+          h(RouteContext.Consumer, {}, ({ params }) => (
+            h(Tabs, {
+              className: 'SideBar-algorithms',
+              onChange: this.onGeneratorChange,
+              selectedTabId: params.generator,
+              vertical: true,
+            },
+              h(Tab, { id: 'depth-first', title: 'Depth-first Search' }),
+              h(Tab, { id: 'kruskals', title: "Kruskal's Algorithm" }),
+              h(Tab, { id: 'prims', title: "Prim's Algorithm" }),
+            )
+          ))
+        );
+      }
+
+      case 'solvers': {
+        return (
+          h(RouteContext.Consumer, {}, ({ params }) => (
+            h(Tabs, {
+              className: 'SideBar-algorithms',
+              onChange: this.onSolverChange,
+              selectedTabId: params.solver,
+              vertical: true,
+            },
+              h(Tab, { id: 'breadth-first', title: 'Breadth-first Search' }),
+              h(Tab, { id: 'wall-follower', title: 'Wall Follower' }),
+              h(Tab, { id: 'dead-end-filling', title: 'Dead-end Filling' }),
+            )
+          ))
+        );
+      }
+
+      default: {
+        throw new Error(`Un-handled case '${this.state.tab}'`);
+      }
+    }
+  }
+
+  render() {
+    return (
+      h('nav', { className: 'SideBar' },
+        h('div', { className: 'SideBar-header' },
+          h('h2', { className: 'bp3-heading' }, 'A-Maze'),
+        ),
+        h(Tabs, { className: 'SideBar-tabs', onChange: this.setTab },
+          h(Tab, { id: 'generators', title: 'Generators' }),
+          h(Tab, { id: 'solvers', title: 'Solvers' }),
+        ),
+        this.renderAlgorithms()
+      )
+    );
+  }
+};
+
