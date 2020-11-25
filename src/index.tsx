5bdf466bbcdd5f617e23cb43c9083d34f04bb749
Tyler Torola <Tyler@TJT.Codes>
2020-11-25 12:14:00 -0500

    Revert "Add basic HMR"
    This reverts commit 2a597014d56efc95ac14c6ff9fdb94ef806a262d.

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,7 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <StrictMode>
-    <div>Hello World!!</div>
+    <div>Hello World</div>
   </StrictMode>,
   document.getElementById("root")
 );
-
-if (module.hot) {
-  module.hot.accept();
-}
